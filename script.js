b6a4c9f4b209ee3c20f18be38048413fd241f85f
tess-phillips <124443397+tess-phillips@users.noreply.github.com>
2023-07-13 19:39:37 +0100

    first commit

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { generateRow } from "./helpers/generateRow.js"
+
 document.addEventListener('DOMContentLoaded',()=>{
     const grid = document.querySelector('.grid');
     const width = 8;
@@ -37,64 +39,7 @@ document.addEventListener('DOMContentLoaded',()=>{
     location.reload();
     });
     
-    function generateRow(){ //can make this shorter with || 
-        var i=0;
-        var row = [];
-        var randomNum= Math.floor(Math.random()*5);
-        while (i<5){
-            randomNum = Math.floor(Math.random()*5);
-            if (randomNum==0){
-                row[i]=randomNum;
-                i +=1;
-            }
-            else{
-                for (let j=0; j< randomNum; j++) {
-                    row[i]=randomNum;
-                    i+=1;
-                }
-            }
-        }
-        if (i==5){
-            randomNum = Math.floor(Math.random()*4);
-            if (randomNum==0){
-                row[i]=randomNum;
-                i +=1;
-            }
-            else{
-                for (let j=0; j< randomNum; j++) {
-                    row[i]=randomNum;
-                    i+=1;
-                }
-            }
-        }
-        if (i==6){
-            randomNum = Math.floor(Math.random()*3);
-            if (randomNum==0){
-                row[i]=randomNum;
-                i +=1;
-            }
-            else{
-                for (let j=0; j< randomNum; j++) {
-                    row[i]=randomNum;
-                    i+=1;
-                }
-            }
-        }
-        if (i==7){
-            randomNum = Math.floor(Math.random()*2);
-            if (randomNum==0){
-                row[i]=randomNum;
-                i +=1;
-            }
-            else{
-                for (let j=0; j< randomNum; j++) {
-                    row[i]=randomNum;
-                    i+=1;
-                }
-            }
-        }
-        if (i>7) {return row;}
-    }
+
     
     function generateRowNotFull(){
         var notFull = false;
