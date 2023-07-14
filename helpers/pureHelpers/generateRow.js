860d8a7cb89010bb59cec62ce2f921546e56bc38
tess-phillips <124443397+tess-phillips@users.noreply.github.com>
2023-07-14 09:53:00 +0100

    generate row improved

--- helpers/pureHelpers/generateRow.js
+++ helpers/pureHelpers/generateRow.js
@@ -1,58 +1,29 @@
-export function generateRow(){ //can make this shorter with || 
-    var i=0;
+export function generateRow() {
     var row = [];
-    var randomNum= Math.floor(Math.random()*5);
-    while (i<5){
-        randomNum = Math.floor(Math.random()*5);
-        if (randomNum==0){
-            row[i]=randomNum;
-            i +=1;
+    var randomNum;
+    var i = 0;
+  
+    while (i < 8) {
+      if (i === 5) {
+        randomNum = Math.floor(Math.random() * 4);
+      } else if (i === 6) {
+        randomNum = Math.floor(Math.random() * 3);
+      } else if (i === 7) {
+        randomNum = Math.floor(Math.random() * 2);
+      } else {
+        randomNum = Math.floor(Math.random() * 5);
+      }
+  
+      if (randomNum === 0) {
+        row[i] = randomNum;
+        i += 1;
+      } else {
+        for (let j = 0; j < randomNum; j++) {
+          row[i] = randomNum;
+          i += 1;
         }
-        else{
-            for (let j=0; j< randomNum; j++) {
-                row[i]=randomNum;
-                i+=1;
-            }
-        }
+      }
     }
-    if (i==5){
-        randomNum = Math.floor(Math.random()*4);
-        if (randomNum==0){
-            row[i]=randomNum;
-            i +=1;
-        }
-        else{
-            for (let j=0; j< randomNum; j++) {
-                row[i]=randomNum;
-                i+=1;
-            }
-        }
-    }
-    if (i==6){
-        randomNum = Math.floor(Math.random()*3);
-        if (randomNum==0){
-            row[i]=randomNum;
-            i +=1;
-        }
-        else{
-            for (let j=0; j< randomNum; j++) {
-                row[i]=randomNum;
-                i+=1;
-            }
-        }
-    }
-    if (i==7){
-        randomNum = Math.floor(Math.random()*2);
-        if (randomNum==0){
-            row[i]=randomNum;
-            i +=1;
-        }
-        else{
-            for (let j=0; j< randomNum; j++) {
-                row[i]=randomNum;
-                i+=1;
-            }
-        }
-    }
-    if (i>7) {return row;}
-}
+  
+    return row;
+  }
